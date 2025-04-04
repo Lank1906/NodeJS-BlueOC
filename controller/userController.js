b31bd38839cf38d9cb0e95bdca4372798a06cb22
Lank1906 <buuixuanhoangc@gmail.com>
2025-04-04 09:51:23 +0700

    do: ex1

---
+++ controller/userController.js
@@ -0,0 +1,32 @@
+const userModel = require('../models/userModel');
+
+exports.createUser = (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+  const newUser = userModel.createUser({ name, email, password });
+  res.status(201).json(newUser);
+};
+
+exports.getAllUsers = (req, res) => {
+  res.json(userModel.getAllUsers());
+};
+
+exports.getUserById = (req, res) => {
+  const user = userModel.getUserById(parseInt(req.params.id));
+  if (!user) return res.status(404).json({ message: 'User not found' });
+  res.json(user);
+};
+
+exports.updateUser = (req, res) => {
+  const updated = userModel.updateUser(parseInt(req.params.id), req.body);
+  if (!updated) return res.status(404).json({ message: 'User not found' });
+  res.json(updated);
+};
+
+exports.deleteUser = (req, res) => {
+  const deleted = userModel.deleteUser(parseInt(req.params.id));
+  if (!deleted) return res.status(404).json({ message: 'User not found' });
+  res.json(deleted);
+};
---
+++ model/userModel.js
@@ -0,0 +1,40 @@
+let users = [];
+let nextId = 1;
+
+function getAllUsers() {
+  return users;
+}
+
+function getUserById(id) {
+  return users.find(user => user.id === id);
+}
+
+function createUser(data) {
+  const newUser = { id: nextId++, ...data };
+  users.push(newUser);
+  return newUser;
+}
+
+function updateUser(id, data) {
+  const user = users.find(u => u.id === id);
+  if (!user) return null;
+
+  Object.assign(user, data);
+  return user;
+}
+
+function deleteUser(id) {
+  const index = users.findIndex(u => u.id === id);
+  if (index === -1) return null;
+
+  const deleted = users.splice(index, 1);
+  return deleted[0];
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
